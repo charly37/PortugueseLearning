a89896260e8a2de813c209e1faa6bef4345bbd04
Charles Walker <charles.walker.37@gmail.com>
2025-12-28 18:50:09 +0000

    nodejs random ipv6 issue - force listen all

--- src/server.ts
+++ src/server.ts
@@ -105,6 +105,6 @@ app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
