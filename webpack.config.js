6dcc7e9f5d84e798ec4ba4feee90b8eba12acf3b
Charles Walker <charles.walker.37@gmail.com>
2026-06-21 09:08:37 +0000

    package update

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,14 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      },
     ],
   },
   plugins: [
